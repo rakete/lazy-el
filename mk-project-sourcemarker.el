7a04b586fa8c9f1a0256fb90087dd6f62af254d0
Andreas Raster <lazor@affenbande.org>
2012-02-22 16:59:26 +0100

    check for non-existing filename/buffer before saving sourcemarker in hooks

--- mk-project-sourcemarker.el
+++ mk-project-sourcemarker.el
@@ -24,10 +24,12 @@
   '(progn
      (add-to-list 'mk-proj-optional-vars 'sourcemarker)
      (add-hook 'mk-proj-after-load-hook 'sourcemarker-visit)
+     (add-hook 'mk-proj-before-unload-hook 'sourcemarker-set)
      (add-hook 'after-save-hook (lambda ()
                                   (when (and (boundp 'mk-proj-name) mk-proj-name
                                              (or (mk-proj-buffer-p (current-buffer))
                                                  (mk-proj-friendly-buffer-p (current-buffer)))
+                                             (buffer-file-name (current-buffer))
                                              (file-exists-p (buffer-file-name (current-buffer))))
                                     (sourcemarker-set))))
      (add-hook 'kill-buffer-hook (lambda ()
@@ -36,40 +38,37 @@
                                               (find-buffer-visiting (cdr (assoc :file mk-proj-sourcemarker)))
                                               (eq (current-buffer) (find-buffer-visiting (cdr (assoc :file mk-proj-sourcemarker)))))
                                      (let ((next-buf (some (lambda (buf)
-                                                                  (when (and (not (eq buf (current-buffer)))
-                                                                             (or (mk-proj-buffer-p buf)
-                                                                                 (mk-proj-friendly-buffer-p buf))
-                                                                             (file-exists-p (buffer-file-name buf)))
-                                                                    buf)) (buffer-list))))
+                                                             (when (and (not (eq buf (current-buffer)))
+                                                                        (or (mk-proj-buffer-p buf)
+                                                                            (mk-proj-friendly-buffer-p buf))
+                                                                        (buffer-file-name buf)
+                                                                        (file-exists-p (buffer-file-name buf)))
+                                                               buf)) (buffer-list))))
                                        (when next-buf
                                          (with-current-buffer next-buf
                                            (sourcemarker-set)))))))
      ))
-
-;; (kill-local-variable 'kill-buffer-hook)
-;; (setq kill-buffer-hook nil)
-
-;; (kill-local-variable 'after-save-hook)
-;; (setq after-save-hook nil)
 
 (defun sourcemarker-visit ()
   "Restore project sourcemarker and go there."
   (interactive)
   (when mk-proj-sourcemarker
     (let* ((m (continue-sourcemarker-restore mk-proj-sourcemarker))
-           (buf (marker-buffer m))
+           (buf (find-file-noselect (cdr (assoc :file mk-proj-sourcemarker))))
            (oldframe (current-frame)))
-      (when (markerp m)
+      (when m
         (when (get-buffer-window (get-buffer buf) 'visible)
           (select-frame (window-frame (get-buffer-window (get-buffer buf) 'visible))))
-        (goto-char (marker-position m))
+        (goto-char m)
         (display-buffer buf)
         (with-current-buffer buf
           (sourcemarker-set))))))
 
 (defun sourcemarker-set ()
-  "Update a projects sourcemarker."
+  "Set/update a projects sourcemarker."
   (interactive)
-  (mk-proj-set-config-val 'sourcemarker (continue-sourcemarker-create)))
+  (when (and (buffer-file-name (current-buffer))
+             (mk-proj-friendly-buffer-p (current-buffer)))
+    (mk-proj-set-config-val 'sourcemarker (continue-sourcemarker-create))))
 
 (provide 'mk-project-sourcemarker)
