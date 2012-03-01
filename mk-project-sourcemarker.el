3539abaab5d242fcae5203396ca8142cc853d58e
Andreas Raster <lazor@affenbande.org>
2012-03-01 01:45:50 +0100

    created sourcemarker-setup function from eval-after-load section, replacing it making setup optional

--- mk-project-sourcemarker.el
+++ mk-project-sourcemarker.el
@@ -20,34 +20,32 @@
 (require 'mk-project)
 (require 'continue)
 
-(eval-after-load "mk-project-sourcemarker"
-  '(progn
-     (add-to-list 'mk-proj-optional-vars 'sourcemarker)
-     (add-hook 'mk-proj-after-load-hook 'sourcemarker-visit)
-     (add-hook 'mk-proj-before-unload-hook 'sourcemarker-set)
-     (add-hook 'after-save-hook (lambda ()
-                                  (when (and (boundp 'mk-proj-name) mk-proj-name
-                                             (or (mk-proj-buffer-p (current-buffer))
-                                                 (mk-proj-friendly-buffer-p (current-buffer)))
-                                             (buffer-file-name (current-buffer))
-                                             (file-exists-p (buffer-file-name (current-buffer))))
-                                    (sourcemarker-set))))
-     (add-hook 'kill-buffer-hook (lambda ()
-                                   (when (and (boundp 'mk-proj-name) mk-proj-name
-                                              (boundp 'mk-proj-sourcemarker) mk-proj-sourcemarker
-                                              (find-buffer-visiting (cdr (assoc :file mk-proj-sourcemarker)))
-                                              (eq (current-buffer) (find-buffer-visiting (cdr (assoc :file mk-proj-sourcemarker)))))
-                                     (let ((next-buf (some (lambda (buf)
-                                                             (when (and (not (eq buf (current-buffer)))
-                                                                        (or (mk-proj-buffer-p buf)
-                                                                            (mk-proj-friendly-buffer-p buf))
-                                                                        (buffer-file-name buf)
-                                                                        (file-exists-p (buffer-file-name buf)))
-                                                               buf)) (buffer-list))))
-                                       (when next-buf
-                                         (with-current-buffer next-buf
-                                           (sourcemarker-set)))))))
-     ))
+(defun sourcemarker-setup ()
+   (add-to-list 'mk-proj-optional-vars 'sourcemarker)
+   (add-hook 'mk-proj-after-load-hook 'sourcemarker-visit)
+   (add-hook 'mk-proj-before-unload-hook 'sourcemarker-set)
+   (add-hook 'after-save-hook (lambda ()
+                                (when (and (boundp 'mk-proj-name) mk-proj-name
+                                           (or (mk-proj-buffer-p (current-buffer))
+                                               (mk-proj-friendly-buffer-p (current-buffer)))
+                                           (buffer-file-name (current-buffer))
+                                           (file-exists-p (buffer-file-name (current-buffer))))
+                                  (sourcemarker-set))))
+   (add-hook 'kill-buffer-hook (lambda ()
+                                 (when (and (boundp 'mk-proj-name) mk-proj-name
+                                            (boundp 'mk-proj-sourcemarker) mk-proj-sourcemarker
+                                            (find-buffer-visiting (cdr (assoc :file mk-proj-sourcemarker)))
+                                            (eq (current-buffer) (find-buffer-visiting (cdr (assoc :file mk-proj-sourcemarker)))))
+                                   (let ((next-buf (some (lambda (buf)
+                                                           (when (and (not (eq buf (current-buffer)))
+                                                                      (or (mk-proj-buffer-p buf)
+                                                                          (mk-proj-friendly-buffer-p buf))
+                                                                      (buffer-file-name buf)
+                                                                      (file-exists-p (buffer-file-name buf)))
+                                                             buf)) (buffer-list))))
+                                     (when next-buf
+                                       (with-current-buffer next-buf
+                                         (sourcemarker-set))))))))
 
 (defun sourcemarker-visit ()
   "Restore project sourcemarker and go there."
