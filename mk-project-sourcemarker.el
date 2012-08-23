c68308e5f626c27e02accdaebf5b4bcad0247e59
Andreas Raster <lazor@affenbande.org>
2012-08-23 15:07:23 +0200

    remove todo sourcemarker stuff that does not really work
    I kind of still want to save a sourcemarker together with
    todos to be able to restore the position where a todo was
    created, but for now just restoring the most recent
    file sourcemarker is good enough

--- mk-project-sourcemarker.el
+++ mk-project-sourcemarker.el
@@ -20,36 +20,10 @@
 (require 'mk-project)
 (require 'continue)
 
-(defun sourcemarker-setup ()
-  (add-to-list 'mk-proj-optional-vars 'sourcemarker)
-  (add-to-list 'mk-proj-internal-vars 'sourcemarker)
-  (add-hook 'mk-proj-after-load-hook 'sourcemarker-visit)
-  ;;(add-hook 'mk-proj-before-unload-hook 'sourcemarker-set)
-  ;; (add-hook 'after-save-hook (lambda ()
-  ;;                              (when (and (boundp 'mk-proj-name) mk-proj-name
-  ;;                                         (or (mk-proj-buffer-p (current-buffer))
-  ;;                                             (mk-proj-friendly-buffer-p (current-buffer)))
-  ;;                                         (buffer-file-name (current-buffer))
-  ;;                                         (file-exists-p (buffer-file-name (current-buffer))))
-  ;;                                (sourcemarker-set))))
-  ;; (add-hook 'kill-buffer-hook (lambda ()
-  ;;                               (when (and (boundp 'mk-proj-name) mk-proj-name
-  ;;                                          (boundp 'mk-proj-sourcemarker) mk-proj-sourcemarker
-  ;;                                          (find-buffer-visiting (cdr (assoc :file mk-proj-sourcemarker)))
-  ;;                                          (eq (current-buffer) (find-buffer-visiting (cdr (assoc :file mk-proj-sourcemarker)))))
-  ;;                                 (let ((next-buf (some (lambda (buf)
-  ;;                                                         (when (and (not (eq buf (current-buffer)))
-  ;;                                                                    (or (mk-proj-buffer-p buf)
-  ;;                                                                        (mk-proj-friendly-buffer-p buf))
-  ;;                                                                    (buffer-file-name buf)
-  ;;                                                                    (file-exists-p (buffer-file-name buf)))
-  ;;                                                           buf)) (buffer-list))))
-  ;;                                   (when next-buf
-  ;;                                     (with-current-buffer next-buf
-  ;;                                       (sourcemarker-set)))))))
-  )
+(defun mk-sourcemarker-setup ()
+  (add-hook 'mk-proj-after-load-hook 'mk-sourcemarker-display-most-recent-buffer))
 
-(defun sourcemarker-display-most-recent-buffer ()
+(defun mk-sourcemarker-display-most-recent-buffer ()
   (let ((buffer (cdar (sort (loop for b in (mk-proj-buffers)
                                   if (and (buffer-file-name b)
                                           (assoc :timestamp (gethash (buffer-file-name b) continue-db nil)))
@@ -61,27 +35,11 @@
         (when (car (mk-proj-buffers))
           (display-buffer (car (mk-proj-buffers))))))))
 
-(defun sourcemarker-visit ()
-  "Restore project sourcemarker and go there."
-  (interactive)
-  (if mk-proj-sourcemarker
-      (let* ((m (continue-sourcemarker-restore mk-proj-sourcemarker))
-             (buf (find-file-noselect (cdr (assoc :file mk-proj-sourcemarker)))))
-        (when m
-          (when (get-buffer-window (get-buffer buf) 'visible)
-            (select-frame (window-frame (get-buffer-window (get-buffer buf) 'visible))))
-          (goto-char m)
-          (let ((display-buffer-reuse-frames t))
-            (display-buffer buf))
-          (with-current-buffer buf
-            (sourcemarker-set))))
-    (sourcemarker-display-most-recent-buffer)))
-
-(defun sourcemarker-set ()
-  "Set/update a projects sourcemarker."
+(defun sourcemarker-create ()
   (interactive)
   (when (and (buffer-file-name (current-buffer))
+             (not (condition-case nil (mk-proj-assert-proj) (error t)))
              (mk-proj-buffer-p (current-buffer)))
-    (mk-proj-set-config-val 'sourcemarker (continue-sourcemarker-create))))
+    (continue-sourcemarker-create)))
 
 (provide 'mk-project-sourcemarker)
